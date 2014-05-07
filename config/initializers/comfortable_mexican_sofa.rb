5539827c07f3f8c2e85d49ecb1db42aa670bc056
Rakesh.P.D <errakeshpd@gmail.com>
2014-05-07 10:47:33 +0530

    footer nav_links added in malayalam

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -2,7 +2,7 @@
 
 ComfortableMexicanSofa.configure do |config|
   # Title of the admin area
-  #   config.cms_title = 'ComfortableMexicanSofa CMS Engine'
+ config.cms_title = 'Arividam CMS Engine'
 
   # Controller that is inherited from CmsAdmin::BaseController
   #   config.base_controller = 'ApplicationController'
