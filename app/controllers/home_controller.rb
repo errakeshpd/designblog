2b2f245949123f519129cfffc78f5f687e3dcac8
Rakesh.P.D <errakeshpd@gmail.com>
2014-05-09 17:01:33 +0530

    nav links fetching from db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
+  before_filter :navdata
+  def navdata
+    @bloglistnav = Cms::Snippet.where('label' => 'adminblogmenus')
+  end
   def index
   end
-
+  def lists
+  end
   def login
   end
 end
